ae5adbe2339dc331a82b8b336243e9530c497e4f
lesajayaweera <166988125+lesajayaweera@users.noreply.github.com>
2024-10-21 22:38:19 +0530

    done completed

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+
+const body = document.querySelector("body");
+
+body.addEventListener("mousemove",(event)=>{
+    console.log("moved");
+    const Xposition = event.offsetX;
+    const yposition = event.offsetY;
+    const spanEl = document.createElement("span");
+    spanEl.style.left = Xposition + "px";
+    spanEl.style.top = yposition + "px";
+
+    const size = Math.random() *100;
+    spanEl.style.width = size + "px";
+    spanEl.style.height = size + "px";
+    body.appendChild(spanEl);
+    setTimeout(() => {
+        spanEl.remove();
+    }, 3000);
+
+
+})
